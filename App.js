b5ae946681c9c957f1f797d6f614eafcafef9ffb
oguzhan18m <62189492+oguzhan18m@users.noreply.github.com>
2020-09-17 20:06:11 +0300

    first

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Image, FlatList, Alert } from "react-native";
+import Header from "./components/Header";
+import ListItem from "./components/ListItem";
+import AddItem from "./components/AddItem";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+   const [items, setItems] = useState([
+      { id: Math.floor(Math.random() * 100), text: "Milk" },
+      { id: Math.floor(Math.random() * 100), text: "Eggs" },
+      { id: Math.floor(Math.random() * 100), text: "Bread" },
+      { id: Math.floor(Math.random() * 100), text: "Juice" },
+   ]);
+
+   const deleteItem = (id) => {
+      setItems((prevItems) => {
+         return prevItems.filter((item) => item.id !== id);
+      });
+   };
+
+   const addItem = (text) => {
+      if (text === "") {
+         Alert.alert("Error", "Please enter an item", { text: "OK" });
+      } else {
+         setItems((prevItems) => {
+            return [
+               { id: Math.floor(Math.random() * 100), text },
+               ...prevItems,
+            ];
+         });
+      }
+   };
+
+   return (
+      <View style={styles.container}>
+         <Header title="Shopping List" />
+         <AddItem addItem={addItem} />
+         <FlatList
+            data={items}
+            renderItem={({ item }) => (
+               <ListItem item={item} deleteItem={deleteItem} />
+            )}
+         />
+      </View>
+   );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+   container: {
+      flex: 1,
+      paddingTop: 60,
+   },
 });
+
+export default App;
